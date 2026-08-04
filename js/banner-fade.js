6a1cb3fbd78b39dfc02fee94923fe2898cdb9f3b
jery7799 <2976844966@qq.com>
2026-08-04 10:21:24 +0800

    修复 banner 轮播（ul position + nsw 三元歧义）

--- js/banner-fade.js
+++ js/banner-fade.js
@@ -8,8 +8,10 @@
         var $lis = $ul.find('li');
         var $ems = $b.find('.ban_t em');
         if ($lis.length < 2) return;
-        // 初始态：第一张显示，其他淡出
-        $lis.css({position: 'absolute', left: 0, top: 0}).hide();
+        // 关键：父容器（ban_p ul）必须是 positioned，li 才能正确在 banner 内叠加
+        $ul.css('position', 'relative');
+        // 初始态：所有 li 绝对定位叠加，第一张显示，其他淡出
+        $lis.css({position: 'absolute', left: 0, top: 0, width: '100%'}).hide();
         $lis.eq(0).show();
         $ems.removeClass('on');
         $ems.eq(0).addClass('on');
